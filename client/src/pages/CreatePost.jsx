24c9df7e1fc6c32fd4b1861ea23d16c220427fa5
chandan9pandey <chandan9pandey@gmail.com>
2024-01-02 14:01:17 +0530

    Revert "implemented backend Dall-E routes"
    This reverts commit 9fe0e410221d31c6ecdb45c0fdbb74a9515a543f.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,28 +17,7 @@ const CreatePost = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-	const generateImage = async () => {
-		if (form.prompt) {
-			try {
-				setGeneratingImg(true);
-				const response = await fetch(`http://localhost:8080/api/v1/dalle`, {
-					method: "POST",
-					headers: {
-						"Content-type": "application/json",
-					},
-					body: JSON.stringify({ prompt: form.prompt }),
-				});
-				const data = await response.json();
-				setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-			} catch (error) {
-				alert(error);
-			} finally {
-				setGeneratingImg(false);
-			}
-		} else {
-			alert("Please provide proper prompt");
-		}
-	};
+	const generateImage = () => {};
 
 	const handleSubmit = () => {};
 	const handleChange = (e) =>
