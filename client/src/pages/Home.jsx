03c4dcd34d2a858454969367181d8a9bf50954b8
chandan9pandey <chandan9pandey@gmail.com>
2024-01-02 19:10:38 +0530

    Home Page & Post routes implemented

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,49 @@ const RenderCards = ({ data, title }) => {
 const Home = () => {
 	const [loading, setLoading] = useState(false);
 	const [allPosts, setAllPosts] = useState(null);
-	const [searchText, setSearchText] = useState("abc");
+	const [searchText, setSearchText] = useState("");
+	const [searchTimeout, setSearchTimeout] = useState(null);
+	const [searchedResults, setSearchedResults] = useState(null);
+	const fetchPosts = async () => {
+		setLoading(true);
+
+		try {
+			const response = await fetch("https://localhost:8080/api/v1/post", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (response.ok) {
+				const result = await response.json();
+				setAllPosts(result.data.reverse());
+			}
+		} catch (err) {
+			alert(err);
+		} finally {
+			setLoading(false);
+		}
+	};
+	useEffect(() => {
+		fetchPosts();
+	}, []);
+
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResult = allPosts.filter(
+					(item) =>
+						item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+						item.prompt.toLowerCase().includes(searchText.toLowerCase())
+				);
+				setSearchedResults(searchResult);
+			}, 500)
+		);
+	};
 	return (
 		<section className="max-w-7xl mx-auto">
 			<div>
@@ -35,7 +77,7 @@ const Home = () => {
 					name="text"
 					placeholder="Search something..."
 					value={searchText}
-					// handleChange={handleSearchChange}
+					handleChange={handleSearchChange}
 				/>
 			</div>
 
@@ -54,9 +96,12 @@ const Home = () => {
 						)}
 						<div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
 							{searchText ? (
-								<RenderCards data={[]} title="No Search Results Found" />
+								<RenderCards
+									data={[searchedResults]}
+									title="No Search Results Found"
+								/>
 							) : (
-								<RenderCards data={[]} title="No Posts Yet" />
+								<RenderCards data={[allPosts]} title="No Posts Yet" />
 							)}
 						</div>
 					</>
